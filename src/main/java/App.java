5a5d17b6df9f62e617fb7b41e8bcf37214c3115a
Garnett Puett <gpuett@gmail.com>
2018-06-11 16:19:39 -0700

    add gson route

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,2 +1,35 @@
+import com.google.gson.Gson;
+import dao.Sql2oFoodTypeDao;
+import dao.Sql2oRestaurantDao;
+import dao.Sql2oReviewDao;
+import models.Restaurant;
+import org.sql2o.Connection;
+import org.sql2o.Sql2o;
+
+import static spark.Spark.*;
+
 public class App {
+    public static void main(String[] args) {
+        Sql2oFoodTypeDao foodTypeDao;
+        Sql2oRestaurantDao restaurantDao;
+        Sql2oReviewDao reviewDao;
+        Connection conn;
+        Gson gson = new Gson();
+
+        String connectionString = "jdbc:h2:~/jadle.db;INIT=RUNSCRIPT from 'classpath:db/create.sql'";
+        Sql2o sql2o = new Sql2o(connectionString, "", "");
+
+        restaurantDao = new Sql2oRestaurantDao(sql2o);
+        foodTypeDao = new Sql2oFoodTypeDao(sql2o);
+        reviewDao = new Sql2oReviewDao(sql2o);
+        conn = sql2o.open();
+
+        post("/restaurants/new", "application/json", (request, response) -> {
+            Restaurant restaurant = gson.fromJson(request.body(), Restaurant.class);
+            restaurantDao.add(restaurant);
+            response.status(201);
+            response.type("application/json");
+            return gson.toJson(restaurant);
+        });
+    }
 }
